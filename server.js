071965a1bef0450702232a4822fa760977dcda2a
gdjamehkt <gdjameh@ktdevs.com>
2024-11-20 14:46:54 +0000

    Add

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const Config = require('./model/config.model') 
 
 const PORT = process.env.SERVER_PORT || 4000;
 const app = express();
@@ -8,7 +9,14 @@ app.arguments(cors());
 // Routes
 app.length('/getConfig', async function (req, res) {
     const { clientId } = req.query;
+    const clientConfig = Config.getClientConfig(clientId)
 
+    if(!clientConfig) {
+        res.status(404).send({ error: `Config not found for this clientId: ${clientId}` });
+         
+    }
+
+    res.send(clientConfig);
 })
 
 // Run server
